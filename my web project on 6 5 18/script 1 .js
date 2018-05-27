f3e1c7d160c10e30ffc15261d5036c5aacbf8a72
soulsam480 <sambitsahoo1100@hotmail.com>
2018-05-27 19:05:43 +0530

    new data
    new data for repo

--- my web project on 6 5 18/script 1 .js
+++ my web project on 6 5 18/script 1 .js
@@ -24,7 +24,14 @@ function showSlides() {
 }
 
 
-
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
 
 
  
