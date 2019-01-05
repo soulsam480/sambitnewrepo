052618cd1b6966935d869d2844eadc2f70d1a10d
sambit sahoo <sambitsahoo1100@hotmail.com>
2019-01-05 23:01:52 +0530

    new data misc
    no update on the portfolio project and just update on the new learnong stuff

---
+++ alphaone/footer.php
@@ -0,0 +1,27 @@
+        <!--the credits-->
+        <div class="footer-pc">
+<div class="credits">
+    <p style="padding:0; margin:0;">
+        Developer: Sambit Sahoo. <br>
+        © 2018-19 All Rights Reserved.
+
+    </p>
+</div>
+        </div>
+       
+    </div >
+    <!-- it ends here-->
+    <footer class="footer-mobile">
+        <div class="credits">
+            <p style="padding:0; margin:0;">
+                Developer: Sambit Sahoo. <br>
+                © 2018-19 All Rights Reserved.
+    
+            </p>
+        </div>
+    
+        </div>
+    </footer>
+</body>
+
+</html>
